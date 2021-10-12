3f1d32ef087baab28e8e9825a1c04435338e39a4
ted <minjagot@naver.com>
2021-10-12 13:55:32 +0900

    커밋 2021-10-12 13:54

---
+++ src/main/java/com/example/board/controller/CommonExceptionAdvice.java
@@ -0,0 +1,28 @@
+package com.example.board.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+@Slf4j
+public class CommonExceptionAdvice {
+
+    @ExceptionHandler(Exception.class)//500 error 를 잡음
+    public String except(Exception e, Model model) {
+        log.error("Exception ......." + e.getMessage());
+        model.addAttribute("exception", e);
+        log.error(model.toString());
+        return "error_page";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class) // 404에러를 잡음
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handle404(NoHandlerFoundException e) {
+        return "error_page";
+    }
+}
